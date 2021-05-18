c1c40e963fc98e48f78680b69517223bb41cb2f9
Carlos Barajas <soldotsol@gmail.com>
2021-05-17 22:58:32 -0700

    Setup the use of subject

--- src/store/chat.js
+++ src/store/chat.js
@@ -1,3 +1,15 @@
 import { Subject } from 'rxjs';
 
 const subject = new Subject();
+
+const initialState = {
+    data: [],
+    newDataCount: 0,
+};
+
+let state = initialState;
+
+const chatStore = {
+    init: () => subject.next(state),
+    subscribe: setState => subject.subscribe(setState)
+}
